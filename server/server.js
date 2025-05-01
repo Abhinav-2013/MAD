f1693e30a2bdce674efe773b1378d53e93cb0606
AkshatBasra <akshatbasra.cs23@bmsce.ac.in>
2025-05-01 23:25:17 +0530

    Added MySQL Connection Pool

--- server/server.js
+++ server/server.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const mysql = require('mysql2');
 require('dotenv').config();
+const pool = require('./db')
+const registerRoute = require('./routes/register');
 
 const app = express();
+app.use(express.json());
 
-// Create a .env file
+// Create a .env file with
+// DB_HOST="localhost"
+// DB_USER="root"
+// DB_PASSWORD="password_here"
+// DB_DATABASE="MAD"
 
 const port = process.env.PORT || 5000;
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE
+// });
 
-connection.connect((err) => {
+pool.connect((err) => {
     if (err) {
       console.error('Error connecting to database: ' + err.stack);
       return;
@@ -23,11 +30,33 @@ connection.connect((err) => {
     console.log('Connected to database with ID ' + connection.threadId);
 });
 
-
+app.use('/register', registerRoute);
+// let currentID = 2;
+// const createID = () => {
+//     return currentID++;
+// }
+//
+// app.post('/register', (req, res) => {
+//     const { name, email, password } = req.body;
+//     const id = createID();
+//     const newUser = [id, name, email, password];
+//     console.log(newUser);
+//     const query = `insert into users (id, name, email, password) VALUES (?, ?, ?, ?)`;
+//     connection.query(query, newUser, (err, result) => {
+//         if (err) {
+//             res.status(500).send({message: `Error creating user: ${err.stack}`});
+//         } else {
+//             res.status(200).send({
+//                 message: 'Successfully registered',
+//                 user: {id, name, email, password}
+//             });
+//         }
+//     })
+// });
 
 app.get('/users', (req, res) => {
-    const query = "SELECT * from users";
-    connection.query(query, (err, result) => {
+    const query = "select * from users";
+    pool.query(query, (err, result) => {
         if(err) {
             res.status(500).send(err);
         } else {
